de01e7a51e20000b25bcbbd3a7cc470a14e362ec
Yoon-Sangwon <tkddnjs1648@gmail.com>
2021-03-19 17:01:06 +0900

    Add radar chart

--- spec_analysis.R
+++ spec_analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(fmsb)
 
 spec <- read.csv(file = "spec_done.csv")
 head(spec)
@@ -43,6 +44,75 @@ summary(aov(activity ~ quantile, data = spec))
 summary(aov(spec_index ~ quantile, data = spec))
 
 
+# radar chart
+spec_3q <- spec %>% filter(quantile == "3q") %>% select("spec_index", "achieve", "language", "skill", "activity") 
+spec_2q <- spec %>% filter(quantile == "2q") %>% select("spec_index", "achieve", "language", "skill", "activity")
+spec_1q <- spec %>% filter(quantile == "1q") %>% select("spec_index", "achieve", "language", "skill", "activity")
+
+q3 <- c(mean(spec_3q$spec_index), mean(spec_3q$achieve), mean(spec_3q$language),
+        mean(spec_3q$skill), mean(spec_3q$activity))
+
+q2 <- c(mean(spec_2q$spec_index), mean(spec_2q$achieve), mean(spec_2q$language),
+        mean(spec_2q$skill), mean(spec_2q$activity))
+
+q1 <- c(mean(spec_1q$spec_index), mean(spec_1q$achieve), mean(spec_1q$language),
+        mean(spec_1q$skill), mean(spec_1q$activity))
+
+range(spec$spec_index)
+max.spec <- c(300, 10, 10, 10, 5)
+min.spec <- c(200, 5, 5, 5, 0)
+
+q3_df <- as.data.frame(rbind(max.spec, min.spec, q3))
+q2_df <- as.data.frame(rbind(max.spec, min.spec, q2))
+q1_df <- as.data.frame(rbind(max.spec, min.spec, q1))
+
+names(q3_df) <- c("스펙지수", "학업성취도", "외국어", "전문능력", "대외활동")
+names(q2_df) <- c("스펙지수", "학업성취도", "외국어", "전문능력", "대외활동")
+names(q1_df) <- c("스펙지수", "학업성취도", "외국어", "전문능력", "대외활동")
+
+?par
+par(mfrow = c(1,3), mar = c(5, 2, 5, 2))
+radarchart(q1_df,
+           pcol='lightcoral',             # 다각형 선의 색 
+           pfcol=rgb(1, 0.5, 0.5, 0.5),  # 다각형 내부 색
+           plwd=3,                       # 다각형 선의 두께
+           cglcol='darkgrey',                # 거미줄의 색
+           cglty=1,                      # 거미줄의 타입
+           cglwd=0.8,                    # 거미줄의 두께
+           axistype=2,                   # 축의 레이블 타입
+           seg = 4,                        # 축의 눈금 분할                         
+           axislabcol = 'black',            # 축의 레이블 색
+           caxislabels = seq(0,10,2.5),    # 축의 레이블 값
+           paxislabels = c(300, 10, 10, 10, 5), 
+           title = "매출액 500억 미만")
+radarchart(q2_df,
+           pcol='mediumaquamarine',             # 다각형 선의 색 
+           pfcol=rgb(0.5, 1, 0.5, 0.5),  # 다각형 내부 색
+           plwd=3,                       # 다각형 선의 두께
+           cglcol='darkgrey',                # 거미줄의 색
+           cglty=1,                      # 거미줄의 타입
+           cglwd=0.8,                    # 거미줄의 두께
+           axistype=2,                   # 축의 레이블 타입
+           seg = 4,                        # 축의 눈금 분할                         
+           axislabcol = 'black',            # 축의 레이블 색
+           caxislabels = seq(0,10,2.5),    # 축의 레이블 값
+           paxislabels = c(300, 10, 10, 10, 5), 
+           title = "매출액 1500억 이상 5000억 미만",)
+radarchart(q3_df,
+           pcol='lightslateblue',             # 다각형 선의 색 
+           pfcol=rgb(0.5, 0.5, 1, 0.5),  # 다각형 내부 색
+           plwd=3,                       # 다각형 선의 두께
+           cglcol='darkgrey',                # 거미줄의 색
+           cglty=1,                      # 거미줄의 타입
+           cglwd=0.8,                    # 거미줄의 두께
+           axistype=2,                   # 축의 레이블 타입
+           seg = 4,                        # 축의 눈금 분할                         
+           axislabcol = 'black',            # 축의 레이블 색
+           caxislabels = seq(0,10,2.5),    # 축의 레이블 값
+           paxislabels = c(300, 10, 10, 10, 5), 
+           title = "매출액 5000억 이상",)
+?radarchart
+
 ggplot(data = spec, aes(x = sales, y = spec_index)) + geom_point() + stat_smooth(col = "red") + coord_cartesian(xlim=c(0, 100000000000))
 ggplot(data = spec, aes(x = sales, y = achieve)) + geom_point() + stat_smooth() + coord_cartesian(ylim=c(0, 100000000000))
 ggplot(data = spec, aes(x = sales, y = language)) + geom_point() + stat_smooth() + coord_cartesian(xlim=c(0, 100000000000))
@@ -73,5 +143,6 @@ summary(lm(sales ~ achieve + language + activity + skill, data = spec_ind))
 range(spec$spec_index)
 summary(spec$spec_index)
 summary(lm(spec_index ~ achieve + language + activity + skill, data = spec))
+
 
 
